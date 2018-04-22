f09861a6e1e9f71bc69e6986c586af9e75493877
bhan <bhan@xiaohongshu.com>
2018-04-22 18:08:02 +0800

    pic optimize

--- public/js/hank2.js
+++ public/js/hank2.js
@@ -21,6 +21,16 @@ setTimeout(function () {
   })
 }, 500)
 
+setTimeout(function () {
+  var domString = ''
+  for (var i = 0; i <= 20; i++) {
+    domString += '<div class="option-wrap"><img src="https://chi-2018.oss-cn-hangzhou.aliyuncs.com/04-22/logo-' + i + '.svg"></div>'
+  }
+  $('.step-3 .input-wrap').append(domString)
+
+  $('.end').append('<img src="https://chi-2018.oss-cn-hangzhou.aliyuncs.com/04-22/00-end.jpg">')
+}, 2000)
+
 $('.cover').click(function () {
   $('.info').animate({ top: 0 });
 })
